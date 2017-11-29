49023964a32f04ce93cbc3606b35aff44e567eea
Simon Glantz <simon.glantz@mac.com>
2017-11-29 21:57:04 +0000

    sg - added expandable sections

--- webapp/WebApplication/Scripts/k9/revealText.js
+++ webapp/WebApplication/Scripts/k9/revealText.js
@@ -1,20 +1,40 @@
-function revealText() {
+function revealText(config) {
 
-    function collapse(logoDiv, els, direction) {
+    function expandCollapse(button) {
+        var container = $(button).closest(".reveal-text");
+        var span = $(button).find("a > span");
+        var icon = $(button).find("i");
 
-    }
+        if (container.hasClass("expanded")) {
+            container.removeClass("expanded");
+            span.html(config.moreText);
+            icon.removeClass("fa-arrow-up");
+            icon.addClass("fa-arrow-down");
 
-    function expand(logoDiv, els) {
-
+            $.fn.scrollToTopOf(container);
+        } else {
+            container.addClass("expanded");
+            span.html(config.lessText);
+            icon.removeClass("fa-arrow-down");
+            icon.addClass("fa-arrow-up");
+        }
     }
 
     function init() {
         $(".reveal-text").each(function () {
             var fadeOutDiv = $("<div class='reveal-text-overlay'>&nbsp;</div>");
-            $(this).appendChild(fadeOutDiv);
-            
+            $(this).append(fadeOutDiv);
+
+            var revealButton =
+                $(
+                    "<div class='reveal-text-button-container text-center'><a class='reveal-text-button'><i class='fa fa-arrow-down'></i> " +
+                    "<span>" + config.moreText + "</span></a></div>");
+            $(this).append(revealButton);
+
+            revealButton.click(function () {
+                expandCollapse(this);
+            });
         });
-        alert("yes");
     }
 
     return {
