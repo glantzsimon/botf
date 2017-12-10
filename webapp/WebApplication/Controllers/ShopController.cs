30751d84890538d0f9b96643626f043a493c1b37
Simon Glantz <simon.glantz@mac.com>
2017-12-10 20:43:01 +0000

    sg - fixed bug in shoip

--- webapp/WebApplication/Controllers/ShopController.cs
+++ webapp/WebApplication/Controllers/ShopController.cs
@@ -9,23 +9,26 @@ using System.Web.Mvc;
 namespace K9.WebApplication.Controllers
 {
     public class ShopController : BaseController
-	{
+    {
 
-		public ShopController(ILogger logger, IDataSetsHelper dataSetsHelper, IRoles roles, IAuthentication authentication, IFileSourceHelper fileSourceHelper)
-			: base(logger, dataSetsHelper, roles, authentication, fileSourceHelper)
-		{
-		}
+        public ShopController(ILogger logger, IDataSetsHelper dataSetsHelper, IRoles roles, IAuthentication authentication, IFileSourceHelper fileSourceHelper)
+            : base(logger, dataSetsHelper, roles, authentication, fileSourceHelper)
+        {
+        }
 
-		public ActionResult Index()
-		{
-		    ViewData[Constants.ViewDataConstants.Locale] =
-		        Thread.CurrentThread.CurrentUICulture.GetMetaLocaleName("_");
-		    return View();
-		}
-		
-		public override string GetObjectName()
-		{
-			return string.Empty;
-		}
-	}
+        public ActionResult Index()
+        {
+            var locale = Thread.CurrentThread.CurrentUICulture.GetMetaLocaleName("_");
+            locale = locale == "en_US" ? "us_US" : locale;
+
+            ViewData[Constants.ViewDataConstants.Locale] = locale;
+
+            return View();
+        }
+
+        public override string GetObjectName()
+        {
+            return string.Empty;
+        }
+    }
 }
