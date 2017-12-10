f6665bd2a16d5da68200ecdf27edfddb69bccca2
Simon Glantz <simon.glantz@mac.com>
2017-12-10 02:13:29 +0000

    sg - remove scrolFadeIn on mobile

--- webapp/WebApplication/Scripts/k9/fadePageIn.js
+++ webapp/WebApplication/Scripts/k9/fadePageIn.js
@@ -9,16 +9,20 @@ function fadePageIn(config)
 
     function initScrollReveal() {
         window.sr = ScrollReveal();
-        sr.reveal(".scrollFadeUp", {
-            duration: 1200,
-            distance: "100px",
-            scale: 1
-        });
-        sr.reveal(".scrollFadeIn", {
-            duration: 1000,
-            distance: "4px",
-            scale: 1
-        });
+        if (window.matchMedia("(min-width: 992px)").matches) {
+            sr.reveal(".scrollFadeUp",
+                {
+                    duration: 1200,
+                    distance: "100px",
+                    scale: 1
+                });
+            sr.reveal(".scrollFadeIn",
+                {
+                    duration: 1000,
+                    distance: "4px",
+                    scale: 1
+                });
+        }
     }
 
     var init = function() {
