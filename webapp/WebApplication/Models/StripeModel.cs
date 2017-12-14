c708903569216575ad70a44a85a13faede3b3d4b
Simon Glantz <simon.glantz@mac.com>
2017-12-14 20:05:13 +0000

    sg - enabled custom error pages

--- webapp/WebApplication/Models/StripeModel.cs
+++ webapp/WebApplication/Models/StripeModel.cs
@@ -1,4 +1,5 @@
-﻿using K9.Globalisation;
+﻿using System;
+using K9.Globalisation;
 using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Threading;
@@ -20,21 +21,42 @@ namespace K9.WebApplication.Models
 
         public double DonationAmountInCents => DonationAmount * 100;
 
-        public string LocalisedCurrencyThreeLetters => new RegionInfo(Thread.CurrentThread.CurrentUICulture.LCID).ISOCurrencySymbol;
+        public string LocalisedCurrencyThreeLetters => GetLocalisedCurrency();
 
         public string Locale => Thread.CurrentThread.CurrentUICulture.TwoLetterISOLanguageName.ToLower();
 
         public string Description { get; set; }
-        
+
         public string StripeToken { get; set; }
+
         public string StripeTokenType	 { get; set; }
+
         public string StripeEmail { get; set; }
+
         public string StripeBillingName { get; set; }
+
         public string StripeBillingAddressCountry { get; set; }
+
         public string StripeBillingAddressCountryCode { get; set; }
+
         public string StripeBillingAddressZip { get; set; }
+
         public string StripeBillingAddressLine1 { get; set; }
+
         public string StripeBillingAddressCity { get; set; }
+
         public string StripeBillingAddressState { get; set; }
+
+        private static string GetLocalisedCurrency()
+        {
+            try
+            {
+                return new RegionInfo(Thread.CurrentThread.CurrentUICulture.LCID).ISOCurrencySymbol;
+            }
+            catch (Exception e)
+            {
+                return "USD";
+            }
+        }
     }
 }
