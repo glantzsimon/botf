443153652280dcedef929762e29b312827a179e2
Simon Glantz <simon.glantz@mac.com>
2017-12-14 03:08:09 +0000

    sg - fixed bug

--- webapp/DataAccess/Models/Donation.cs
+++ webapp/DataAccess/Models/Donation.cs
@@ -1,14 +1,12 @@
 ﻿using K9.Base.DataAccessLayer.Attributes;
 using K9.Base.DataAccessLayer.Models;
+using K9.Base.Globalisation;
 using System;
 using System.ComponentModel.DataAnnotations;
-using K9.Base.Globalisation;
-using K9.SharedLibrary.Authentication;
 
 namespace K9.DataAccessLayer.Models
 {
     [AutoGenerateName]
-    [DefaultPermissions(Role = RoleNames.Administrators)]
     [Name(ResourceType = typeof(K9.Globalisation.Dictionary), ListName = Globalisation.Strings.Names.Donations, PluralName = Globalisation.Strings.Names.Donations)]
     public class Donation : ObjectBase
 	{
