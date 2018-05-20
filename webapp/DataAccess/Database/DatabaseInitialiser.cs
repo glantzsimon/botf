2b01a33f9cfbb1504bae1ad19a4a74a004d63089
= <=>
2018-05-20 17:24:37 +0100

    sg - update

--- webapp/DataAccess/Database/DatabaseInitialiser.cs
+++ webapp/DataAccess/Database/DatabaseInitialiser.cs
@@ -1,9 +1,18 @@
-﻿using K9.Base.DataAccessLayer.Database;
+﻿using K9.Base.DataAccessLayer.Config;
+using K9.Base.DataAccessLayer.Database;
+using K9.SharedLibrary.Helpers;
+using System.Configuration;
 
 namespace K9.DataAccessLayer.Database
 {
     public class DatabaseInitialiserLocal : DatabaseInitialiser<LocalDb>
 	{
+	    public DatabaseInitialiserLocal()
+	    {
+	        var dbConfig = ConfigHelper.GetConfiguration<DatabaseConfiguration>(ConfigurationManager.AppSettings).Value;
+	        AutomaticMigrationDataLossAllowed = dbConfig.AutomaticMigrationDataLossAllowed;
+	    }
+
         protected override void Seed(LocalDb db)
 		{
             base.Seed(db);
