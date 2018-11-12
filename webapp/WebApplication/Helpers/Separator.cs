7a73329cc03a1db4efac4be0fdab52d5e2453651
= <=>
2018-11-12 03:50:35 +0000

    sg - membership

--- webapp/WebApplication/Helpers/Separator.cs
+++ webapp/WebApplication/Helpers/Separator.cs
@@ -1,14 +1,18 @@
 ﻿using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using K9.WebApplication.Options;
 
 namespace K9.WebApplication.Helpers
 {
     public static partial class HtmlHelpers
 	{
 
-		public static MvcHtmlString Separator(this HtmlHelper html)
+		public static MvcHtmlString Separator(this HtmlHelper html, bool hideLastOfType = false)
 		{
-			return html.Partial("_Separator");
+			return html.Partial("_Separator", new SeparatorOptions
+			{
+                IsHideLastOfType = hideLastOfType
+			});
 		}
 
 	}
---
+++ webapp/WebApplication/Options/SeparatorOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace K9.WebApplication.Options
+{
+    public class SeparatorOptions
+    {
+        public bool IsHideLastOfType { get; set; }
+
+        public string CssClass => !IsHideLastOfType ? "show-last" : string.Empty;
+    }
+}
